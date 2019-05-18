c2db44e924425aaf831abec25c3ba6f438964198
trusohamn <marta.trusohamn@gmail.com>
2019-05-18 16:45:04 +0200

    config mongo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8080;
 const server = http
     .createServer((req, res) => {
         const path = req.url.match(/\/[^\/?]*/)[0];
-        res.end(url ? 'mongo' : 'no url');
+        res.end(url);
         try {
             switch (path) {
                 case '/': {
