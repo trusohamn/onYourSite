9e916d7a53e5833037e5d80dcfbb2b9f7725af24
trusohamn <marta.trusohamn@gmail.com>
2019-05-27 09:26:05 +0200

    refactored newprofile profiledata

--- newProfile/index.js
+++ newProfile/index.js
@@ -1,28 +1,29 @@
-const add = process.env.NODE_ENV === 'production' ? addDBPromise : addMockPromise;
+const add =
+  process.env.NODE_ENV === 'production' ? addDBPromise : addMockPromise;
 
 const MongoClient = require('mongodb').MongoClient;
 const url = process.env.MONGOLAB_URI;
 
 function addDBPromise(entry) {
-    return new Promise((resolve, reject) => {
-        MongoClient.connect(url, (err, db) => {
-            if (err) reject(err);
-            var dbo = db.db("mydb");
-            dbo.collection("test").insertOne(entry, (err, res) => {
-                if (err) reject(err);
-                console.log("1 document inserted");
-                db.close();
-                resolve('/profile/' + entry._id);
-            });
-        });
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(url, (err, db) => {
+      if (err) return reject(err);
+      var dbo = db.db('mydb');
+      dbo.collection('test').insertOne(entry, (err) => {
+        if (err) return reject(err);
+        console.log('1 document inserted');
+        db.close();
+        return resolve('/profile/' + entry._id);
+      });
     });
+  });
 }
 
-function addMockPromise(key) {
-    console.log('calling addMock');
-    return new Promise((resolve, reject) => {
-        resolve('/profile/1');
-    })
+function addMockPromise() {
+  console.log('calling addMock');
+  return new Promise((resolve) => {
+    resolve('/profile/1');
+  });
 }
 
 module.exports.add = add;
